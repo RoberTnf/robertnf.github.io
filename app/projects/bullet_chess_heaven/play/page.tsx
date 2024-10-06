140d067532de88e6cbeb13a7e69d6e38d7afb6a6
Roberto Diaz Perez <robertodiaztnf@gmail.com>
2024-10-06 15:36:34 +0100

    Rely on pages from bch CICD

--- app/projects/bullet_chess_heaven/play/page.tsx
+++ app/projects/bullet_chess_heaven/play/page.tsx
@@ -4,6 +4,6 @@ export default async function BulletChessHeavenGame() {
 	return <div>
 		<h1>Bullet Chess Heaven</h1>
 		<p className="wip">This game is heavily WIP, follow the <Link href="/blog">blog</Link> for updates. Check out the code in <a href="https://github.com/RoberTnf/bullet_chess_heaven_rs">GitHub</a>.</p>
-		<iframe id="mygame-iframe" src="/bullet_chess_heaven_rs/wasm/index.html" width="1280" height="720" className="overflow-hidden" scrolling="no"></iframe>
+		<iframe id="bch-iframe" src="https://robertnf.github.io/bullet_chess_heaven_rs/" width="1280" height="720" className="overflow-hidden" scrolling="no"></iframe>
 	</div>;
 }
