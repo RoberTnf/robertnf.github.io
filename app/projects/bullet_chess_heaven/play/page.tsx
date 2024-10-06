29b7cf63d665e24bd68f9d44f01f5c8ccb3008e2
Roberto Diaz Perez <robertodiaztnf@gmail.com>
2024-10-06 20:27:37 +0100

    Mobile device disclaimer

--- app/projects/bullet_chess_heaven/play/page.tsx
+++ app/projects/bullet_chess_heaven/play/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default async function BulletChessHeavenGame() {
 	return <div>
 		<h1>Bullet Chess Heaven</h1>
-		<p className="wip">This game is heavily WIP, follow the <Link href="/blog">blog</Link> for updates. Check out the code in <a href="https://github.com/RoberTnf/bullet_chess_heaven_rs">GitHub</a>.</p>
+		<p className="wip">This game is heavily WIP, follow the <Link href="/blog">blog</Link> for updates. It does not work on mobile devices (yet). Check out the code in <a href="https://github.com/RoberTnf/bullet_chess_heaven_rs">GitHub</a>.</p>
 		<iframe id="bch-iframe" src="https://robertnf.github.io/bullet_chess_heaven_rs/" width="1280" height="720" className="overflow-hidden" scrolling="no"></iframe>
 	</div>;
 }
