eaf64921d1cc5580e58b9deb99aefcd5a8e8a3c7
Roberto Diaz Perez <robertodiaztnf@gmail.com>
2024-10-16 20:56:31 +0100

    update email

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,19 @@
+'use client'
+
+import { useState, useEffect } from 'react';
+
 export default function Contact() {
-	return <div>
-		<h1>Contact me</h1>
-		<p>You can contact me at <span className="emphasis">robertotnf at gmail dot com</span>.</p>
-	</div>
+    const [email, setEmail] = useState('');
+
+    useEffect(() => {
+        const parts = ['rob', 'erto', 'diaz', 'tnf', ' at ', 'gma', 'il', ' dot ', 'com'];
+        setEmail(parts.join(''));
+    }, []);
+
+    return <div>
+        <h1>Contact me</h1>
+        <p>You can contact me at: <span className="email-address">
+            {email || 'Loading...'}
+        </span></p>
+    </div>
 }
