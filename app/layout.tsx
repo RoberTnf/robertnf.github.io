0bfda4b4a1ff1a7d5e2d84da2dc93cd75caa2f6d
Roberto Diaz Perez <robertodiaztnf@gmail.com>
2024-10-17 06:19:29 +0100

    Disable dark reader

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,47 +5,50 @@ import Nav from "./components/nav";
 import Footer from "./components/footer";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+    src: "./fonts/GeistVF.woff",
+    variable: "--font-geist-sans",
+    weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+    src: "./fonts/GeistMonoVF.woff",
+    variable: "--font-geist-mono",
+    weight: "100 900",
 });
 
 export const metadata: Metadata = {
-  title: "robertnf",
-  description: "My personal website",
-  icons: {
-    icon: [
-      { url: '/favicon.ico' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-    ],
-    apple: [
-      { url: '/apple-touch-icon.png' },
-    ],
-  },
+    title: "robertnf",
+    description: "My personal website",
+    icons: {
+        icon: [
+            { url: '/favicon.ico' },
+            { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+            { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+        ],
+        apple: [
+            { url: '/apple-touch-icon.png' },
+        ],
+    },
+    other: {
+        "darkreader-lock": "disable dark reader",
+    }
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased font-mono bg-background min-h-screen`}
-      >
-        <div className="max-w-screen-lg mx-auto px-2">
-          <Nav />
-          {children}
-          <Footer />
-        </div>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased font-mono bg-background min-h-screen`}
+            >
+                <div className="max-w-screen-lg mx-auto px-2">
+                    <Nav />
+                    {children}
+                    <Footer />
+                </div>
+            </body>
+        </html>
+    );
 }
