9d7f3e8a16ff0be4129db3253dafc49d7b3e35bd
Roberto Diaz <robertodiaztnf@gmail.com>
2026-01-05 22:14:27 +0000

    direnv

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Next.js development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_22
+            pnpm
+            nodePackages.typescript
+            nodePackages.typescript-language-server
+          ];
+
+          shellHook = ''
+            echo "Next.js dev environment ready"
+            echo "Run 'pnpm install' then 'pnpm dev' to start"
+          '';
+        };
+      }
+    );
+}
